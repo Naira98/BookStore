0de5d36ef9d5c4dc608093b7fbf77b9786691efd
Naira98 <nairamm99@gmail.com>
2024-09-08 15:51:58 +0300

    replace sessions with jwt

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,22 +1,12 @@
 import path from "path";
-import express, { NextFunction, Request, Response } from "express";
-import mongoose, { ObjectId } from "mongoose";
-import session from "express-session";
-import { default as connectMongoDBSession } from "connect-mongodb-session";
+import express from "express";
+import mongoose from "mongoose";
 import { config } from "./config/config";
-
 import authRoutes from "./routes/auth";
 import adminRoutes from "./routes/admins";
 import userRoutes from "./routes/users";
-import User, { IUserModel } from "./models/User";
 
 const app = express();
-const mongoDBStore = connectMongoDBSession(session);
-
-const store = new mongoDBStore({
-  uri: config.mongo.url,
-  collection: "sessions",
-});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -34,40 +24,6 @@ declare module "express" {
     user?: { userId?: string; type?: string };
   }
 }
-
-// declare module "express-session" {
-//   interface SessionData {
-//     userId: ObjectId;
-//     isAuth: boolean;
-//     type: string;
-//   }
-// }
-
-// app.use(
-//   session({
-//     secret: config.sessions.secret,
-//     resave: false,
-//     saveUninitialized: true,
-//     store,
-//   })
-// );
-
-// app.use(async (req: Request, res: Response, next: NextFunction) => {
-//   try {
-//     // console.log(req.session)
-//     if (!req.session.userId) {
-//       next();
-//     } else {
-//       const user = await User.findById(req.session.userId).select("-password");
-//       if (!user) next();
-//       req.user = user;
-//       next();
-//     }
-//   } catch (error) {
-//     console.log(error);
-//     return res.status(500).json(error);
-//   }
-// });
 
 app.use("/auth", authRoutes);
 app.use("/admins", adminRoutes);
